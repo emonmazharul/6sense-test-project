e39bc089c3f350adcdcc8928c68909a1e8dbc5f5
emonmazharul <emonmazharul@yandex.com>
2022-07-14 16:30:43 +0600

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './upwork-test-project/app';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/upwork-test-project/typescript/content.tsx
+++ src/upwork-test-project/typescript/content.tsx
@@ -12,8 +12,8 @@ interface Props {
 
 const Content:React.FC<Props> = ({name,surename,email,age,gender,subscribe,favouriteColor}) =>  {
   // here subsribe mean notification alert
-  const girl_or_body = gender === 'Male' ? 'Guy' : 'Girl';
-  let subscribe_msg = subscribe ? 'on' : 'off';
+  const girl_or_body:string = gender === 'Male' ? 'Guy' : 'Girl';
+  const subscribe_msg:string = subscribe ? 'on' : 'off';
   return <div>
     <h2>My name is {name+' ' + surename}</h2>
     <p>I am a {age} years old {girl_or_body}.My favourite color is {favouriteColor} </p>
